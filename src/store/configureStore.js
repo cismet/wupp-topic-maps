7ed4adb4ea25817545ba3b35db031940ef4b7494
helllth <th@cismet.de>
2017-05-05 11:12:22 +0200

    working on #1: first try. no luck -.-

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,9 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import * as UiStateActions from '../actions/uiStateActions'
+import { routerForBrowser } from 'redux-little-router';
 
 import logger from 'redux-logger';
 
@@ -22,23 +23,61 @@ function configureStoreProd(initialState) {
 }
 
 function configureStoreDev(initialState) {
+  const routes = {
+  '/messages': {
+    title: 'Message'
+  },
+  '/messages/:user': {
+    title: 'Message History'
+  },
+  // You can also define nested route objects!
+  // Just make sure each route key starts with a slash.
+  '/': {
+    title: 'Home',
+    '/bio': {
+      title: 'Biographies',
+      '/:name': {
+        title: 'Biography for:'
+      }
+    }
+  }
+};
+
+  const {
+  reducer,
+  routerMiddleware,
+  enhancer
+} = routerForBrowser({
+  // The configured routes. Required.
+  routes,
+  // The basename for all routes. Optional.
+  basename: '/example'
+})
+
+
   const middlewares = [
     // Add other middleware on this line...
     logger,
     // Redux middleware that spits an error on you when you try to mutate your state either inside a dispatch or between dispatches.
     reduxImmutableStateInvariant(),
-
+    routerMiddleware, 
     // thunk middleware can also accept an extra argument to be passed to each thunk action
     // https://github.com/gaearon/redux-thunk#injecting-a-custom-argument
     thunk,
   ];
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  const store = createStore(rootReducer, initialState, composeEnhancers(
+  const store = createStore(combineReducers({ router: reducer, rootReducer }), initialState, composeEnhancers(enhancer,
     applyMiddleware(...middlewares)
     )
   );
-
+  /*
+  const clientOnlyStore = createStore(
+    combineReducers({ router: reducer, yourReducer }),
+    initialState,
+    compose(enhancer, applyMiddleware(middleware))
+  );
+  */
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {

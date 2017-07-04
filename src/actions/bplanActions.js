76c12cd9efccab8ae618539c8c017b3a60e1ad6c
helllth <th@cismet.de>
2017-07-04 09:49:32 +0200

    working on #5: removed conmsole.log

--- src/actions/bplanActions.js
+++ src/actions/bplanActions.js
@@ -60,7 +60,6 @@ export function searchForPlans(gazObject) {
 
                 if (gazObject!=null && gazObject.length == 1 && gazObject[0] !=null && gazObject[0].string==feature.properties.nummer) {
 //                  if (gazObject!=null && gazObject[0].string==feature.properties.nummer) {
-                  console.log("selectionwish="+counter)
                   selectionIndexWish=counter;
                 }
                 featureArray.push(feature);

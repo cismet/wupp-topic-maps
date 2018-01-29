2ad592b5c37652f2c78d40bdebfd5506d9708d3f
helllth <th@cismet.de>
2018-01-29 11:40:16 +0100

    :bug: fixed the problem when going to the default page

--- src/containers/DefaultPage.js
+++ src/containers/DefaultPage.js
@@ -1,36 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Cismap from '../containers/Cismap';
-import { connect } from "react-redux";
-import { Well } from 'react-bootstrap';
+import {connect} from "react-redux";
+import {Well} from 'react-bootstrap';
 import Control from 'react-leaflet-control';
 
 function mapStateToProps(state) {
-  return {
-    ui: state.uiState,
-  };
+    return {ui: state.uiState};
 }
 export class DefaultPage_ extends React.Component {
-  render() {   
-   return (
-        <div>
-            <Cismap>
-            <Control position="bottomright" >
-                <Well bsSize="small">
-                    <h2>Default</h2>
-                    </Well>
-            </Control>
-                </Cismap>
-        </div>
-    );
-  }
+    render() {
+        let info = (
+            <Well pixelwidth={150} bsSize="small">
+                <h2>Default</h2>
+            </Well>
+        );
+        return (
+            <div>
+                <Cismap infoBox={info}/>                
+            </div>
+        );
+    }
 }
 const DefaultPage = connect(mapStateToProps)(DefaultPage_);
 
 export default DefaultPage;
 
 DefaultPage_.propTypes = {
-  ui: PropTypes.object,
-  kassenzeichen: PropTypes.object,
-  uiState: PropTypes.object
+    ui: PropTypes.object,
+    kassenzeichen: PropTypes.object,
+    uiState: PropTypes.object
 };

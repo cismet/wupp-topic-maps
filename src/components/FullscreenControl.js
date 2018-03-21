24bdc12c7e0d9e3af7c295478ae3cf859219cd86
helllth <th@cismet.de>
2018-03-21 14:46:19 +0100

    working on ä20: fixing fullscreen problems in severeal browsers. uses a fork of Leaflet.fullscreen in that commit

--- src/components/FullscreenControl.js
+++ src/components/FullscreenControl.js
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import { MapControl } from 'react-leaflet';
 import L from 'leaflet';
 
-import 'leaflet-fullscreen/dist/leaflet.fullscreen.css';
-import 'leaflet-fullscreen';
+import 'leaflet-fullscreen-custom-container-fork/dist/leaflet.fullscreen.css';
+import 'leaflet-fullscreen-custom-container-fork';
 
 
 class FullscreenControl extends MapControl {
@@ -17,7 +17,8 @@ class FullscreenControl extends MapControl {
       content:this.props.content,
       forceSeparateButton:this.props.forceSeparateButton,
       forcePseudoFullscreen:this.props.forcePseudoFullscreen,
-      fullscreenElement:this.props.fullscreenElement
+      fullscreenElement:this.props.fullscreenElement,
+      container:this.props.container
     });
   }
 }

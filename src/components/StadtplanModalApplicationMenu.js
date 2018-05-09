427922981684caeac2a95f9047bd6b960dbe4117
helllth <th@cismet.de>
2018-05-09 12:32:07 +0200

    :page_facing_up: #18: updated the licensing info in the app menus of Ehrenamt and Statdplan

--- src/components/StadtplanModalApplicationMenu.js
+++ src/components/StadtplanModalApplicationMenu.js
@@ -353,9 +353,13 @@ export class StadtplanModalApplicationMenu_ extends React.Component {
                                     style={{
                                     fontSize: "11px"
                                 }}>
-                                    <b>Hintergrundkarte</b>: <a>Stadtplanwerk 2.0 (Beta)</a>, <Icon name="copyright"/> Regionalverband Ruhr (RVR) und Kooperationspartner.<br/>
+                                  <b>Hintergrundkarte</b>: <a>Stadtplanwerk 2.0 (Beta)</a>, <Icon name="copyright"/> Regionalverband Ruhr (RVR) und Kooperationspartner,
+                                    Datengrundlagen <Icon name="copyright"/> OpenStreetMap contributors (<a href="http://www.opendatacommons.org/licenses/odbl/1.0/" target="_licensing">ODbL</a>) und <Icon name="copyright"/> Land NRW (2018) 
+                                    , <a href="http://www.govdata.de/dl-de/by-2-0" target="_licensing">Datenlizenz Deutschland - Namensnennung - Version 2.0</a>.<br/>
+                                    
                                     <b>Technische Realisierung</b>: <a href="https://cismet.de/" target="_cismet">cismet GmbH</a> auf Basis von <a  href="http://leafletjs.com/" target="_leaflet">Leaflet</a> und <a href="https://cismet.de/#refs" target="_cismet">cids | WuNDa</a><br/>
-                                    <b>POI-Daten</b>: {this.props.offersMD5}</span>
+                                    {/* <b>POI-Daten</b>: {this.props.offersMD5} */}
+                                </span>
                                     
                             </td>
                             <td>

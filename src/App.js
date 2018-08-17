d63e089a5f2e17aecf32832a20292ae3382d1e6e
helllth <th@cismet.de>
2018-08-17 10:55:47 +0200

    :sparkles: working on #28: add route for kitas

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import DefaultPage from './containers/DefaultPage';
 import BPlaene from './containers/BPlaene';
 import Ehrenamt from './containers/Ehrenamt';
 import Stadtplan from './containers/Stadtplan';
+import Kitas from './containers/Kitas';
 import Experiments from './containers/Experiments';
 
 import store from './redux/store';
@@ -46,6 +47,7 @@ export default class App extends React.Component {
                 <Route exact path="/ehrenamt/:layers?/:offerid?" component={Ehrenamt} />
                 <Route exact path="/bplaene/:layers?/:bplannummer?" component={BPlaene} />
                 <Route exact path="/stadtplan/:layers?/" component={Stadtplan} />
+                <Route exact path="/kitas/:layers?/" component={Kitas} />
                 <Route exact path="/experiments" component={Experiments}/>
                 <Route exact path="/default(/:layers)(/:bplannummer)" component={DefaultPage}/>
                 <Route component={NotFoundPage} />

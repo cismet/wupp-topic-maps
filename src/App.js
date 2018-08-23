74b444896ecb4b9d1b327a98ea1106634c54d8b9
helllth <th@cismet.de>
2018-08-23 09:36:22 +0200

    :sparkles: working on ä28: added a RESET route to get a defined starting change after incompatible redux-state changes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Ehrenamt from './containers/Ehrenamt';
 import Stadtplan from './containers/Stadtplan';
 import Kitas from './containers/Kitas';
 import Experiments from './containers/Experiments';
+import Reset from './containers/Reset';
 
 import store from './redux/store';
 import ReactLoading from 'react-loading';
@@ -50,6 +51,8 @@ export default class App extends React.Component {
                 <Route exact path="/kitas/:layers?/" component={Kitas} />
                 <Route exact path="/experiments" component={Experiments}/>
                 <Route exact path="/default(/:layers)(/:bplannummer)" component={DefaultPage}/>
+                <Route exact path="/reset" component={Reset}/>
+                
                 <Route component={NotFoundPage} />
               </Switch>
             </main>

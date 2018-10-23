d215d35db09892cdcab8f252d125308079250300
helllth <th@cismet.de>
2018-10-23 15:17:54 +0200

    :text: wo #32: correcting the legal map footer

--- src/components/commons/GenericRVRStadtplanwerkMenuFooter.js
+++ src/components/commons/GenericRVRStadtplanwerkMenuFooter.js
@@ -6,9 +6,9 @@ const GenericRVRStadtplanwerkMenuFooter = () => {
     <div>
       <span style={{ fontSize: "11px" }}>
       {/* Hintergrundkarte: Stadtplanwerk 2.0 (Beta) © Regionalverband Ruhr und Kooperationspartner (CC BY 4.0), Datengrundlagen © Land NRW (2018), Datenlizenz Deutschland - Namensnennung - Version 2.0 und OpenStreetMap contributors (ODbL) */}
-        <b>Hintergrundkarte</b>: <a>Stadtplanwerk 2.0 (Beta)</a>, <Icon name="copyright" />{" "}
+        <b>Hintergrundkarte</b>: <a>Stadtplanwerk 2.0 (Beta)</a> <Icon name="copyright" />{" "}
         Regionalverband Ruhr (RVR) und Kooperationspartner (<a href="https://creativecommons.org/licenses/by/4.0/legalcode.de"  target="_licensing">CC BY 4.0</a>), Datengrundlagen{" "}
-        <Icon name="copyright" /> Land NRW (2018), <a href="http://www.govdata.de/dl-de/by-2-0" target="_licensing">
+        <Icon name="copyright" /> Land NRW (2018) <a href="http://www.govdata.de/dl-de/by-2-0" target="_licensing">
           Datenlizenz Deutschland - Namensnennung - Version 2.0
         </a> und OpenStreetMap contributors (
         <a href="http://www.opendatacommons.org/licenses/odbl/1.0/" target="_licensing">

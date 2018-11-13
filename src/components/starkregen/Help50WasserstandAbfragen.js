b769311960fb0af6339aff8ca35d086e756363fa
helllth <th@cismet.de>
2018-11-13 13:47:45 +0100

    :text: wo #33: added help text got get feature info

--- src/components/starkregen/Help50WasserstandAbfragen.js
+++ src/components/starkregen/Help50WasserstandAbfragen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import GenericModalMenuSection from '../commons/GenericModalMenuSection';
-
+import { Icon } from 'react-fa';
 const Component = ({ uiState, uiStateActions }) => {
 	return (
 		<GenericModalMenuSection
@@ -12,12 +12,28 @@ const Component = ({ uiState, uiStateActions }) => {
 			sectionContent={
 				<div>
 					<p>
-						Und es war ihnen wie eine Bestätigung ihrer neuen Träume und guten Absichten, als am Ziele ihrer
-						Fahrt die Tochter als erste sich erhob und ihren jungen Körper dehnte. »Es ist ein
-						eigentümlicher Apparat«, sagte der Offizier zu dem Forschungsreisenden und überblickte mit einem
-						gewissermaßen bewundernden Blick den ihm doch wohlbekannten Apparat. Sie hätten noch ins Boot
-						springen können, aber der Reisende hob ein schweres, geknotetes Tau vom Boden, drohte ihnen
-						damit und hielt sie dadurch von dem Sprunge ab.
+						Durch Anklicken der Schaltfläche <Icon name="crosshairs" /> oberhalb des Kontrollfelds
+						aktivieren Sie den Modus zur Abfrage des maximalen Wasserstandes, der im Verlauf einer
+						Simulation für eine jede 1m x 1m Rasterzelle berechnet worden ist. Anstelle der Schaltfläche
+						erscheint in diesem Modus das Anzeigefeld "Maximaler Wasserstand", zunächst mit einem kurzen
+						Bedienungshinweis. Ein Klick auf eine beliebige Position in der Karte bewirkt jetzt, dass die
+						Zelle in der Karte markiert und der zugehörige maximale Wasserstand in diesem Feld angezeigt
+						wird. Die Anzeige wird dabei auf volle 10 cm gerundet (z. B. "ca. 90 cm"), um die{' '}
+						<a onClick={() => uiStateActions.showApplicationMenuAndActivateSection(true, 'aussagekraft')}>
+							beschränkte Aussagekraft der Simulationsergebnisse
+						</a>{' '}
+						zu verdeutlichen. Aus demselben Grund werden berechnete Wasserstände von mehr als 150 cm nur als
+						"> 150 cm" angezeigt. Wenn Sie nach der Abfrage eines maximalen Wasserstandes eine andere
+						Simulation auswählen, wird der angezeigte Zellwert automatisch aktualisiert. So können Sie für
+						eine bestimmte Position bequem alle angebotenen Simulations-Szenarien durchgehen. Auch im
+						Abfragemodus können Sie die Karte mit gedrückter linker Maustaste verschieben. Wenn Sie auf
+						diese Weise oder durch{' '}
+						<a onClick={() => uiStateActions.showApplicationMenuAndActivateSection(true, 'positionieren')}>
+							Positionierung über einen Suchbegriff
+						</a>{' '}
+						einen Kartenausschnitt auswählen, in dem die zuletzt abgefragte Zelle nicht mehr enthalten ist,
+						wird das Anzeigefeld auf seinen Startzustand zurückgesetzt. Mit einem Klick auf das <Icon name="close" /> Symbol
+						rechts oben im Anzeigefeld beenden Sie den Abfragemodus.
 					</p>
 				</div>
 			}

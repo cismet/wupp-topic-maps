a9b6358dec854c2a62cd90afdd2bfb01faf0a7af
helllth <th@cismet.de>
2019-03-15 15:43:32 +0100

    :text: wo #34: help text changes due to new features

--- src/components/BPlanModalHelpComponent.js
+++ src/components/BPlanModalHelpComponent.js
@@ -167,67 +167,56 @@ export class BPlanModalHelp_ extends React.Component {
 							Treffermenge vollst&auml;ndig angezeigt werden.
 						</Panel>
 						<Panel header="B-Plan-Dokumente betrachten" eventKey="7" bsStyle="info">
-							<p>
-								Durch Anklicken des Links "Dokumente" in der Info-Box oder des PDF-Symbols direkt
-								darüber wird in einer neuen Registerkarte Ihres Browsers ein Dokumentenviewer geöffnet,
-								in dem die Dokumente zu demjenigen B-Plan betrachtet werden können, der gerade den Fokus
-								hat. Wenn zu dem B-Plan mehrere Dokumente verfügbar sind, werden diese in einer
-								Navigationsleiste am linken Rand des Dokumentenviewers angeboten. Klicken Sie auf eines
-								der Symbole in der Navigationsleiste, um das zugehörige Dokument in den Anzeigebereich
-								des Dokumentenviewers zu laden. Mit den Werkzeugen <Icon name="chevron-left" />{' '}
-								<i>"vorherige Seite"</i> und <Icon name="chevron-right" /> <i>"nächste Seite"</i> in der
-								Werkzeugleiste am oberen Rand des Dokumentenviewers können Sie in mehrseitigen
-								Dokumenten vor- und zurückblättern.
-							</p>
-							<p>
-								Mehrere Dokumente werden angezeigt, wenn der B-Plan mehrere Planteile umfasst oder wenn
-								die verfahrensbegleitenden Zusatzdokumente bereits in digitaler Form vorliegen. Die
-								Bereitstellung dieser Dokumente ist ein laufendes Vorhaben der Stadtverwaltung
-								Wuppertal, Stand 03/2019 sind sie für rund 60% der Wuppertaler B-Pläne verfügbar. Die
-								Namenskonventionen für die vielfältigen Zusatzdateien sind im Dokument{' '}
-								<a href={INFO_DOC_DATEINAMEN_URL} target="_info">
-									Info Dateinamen
-								</a>{' '}
-								beschrieben, das in der Navigationsleiste als oberstes Zusatzdokument angeboten wird.
-							</p>
-							<p>
-								Im Anzeigebereich können Sie das Dokument durch Ziehen mit der Maus verschieben. Mit den
-								Werkzeugen <Icon name="plus" /> und <Icon name="minus" /> können Sie die Darstellung
-								vergrößern bzw. verkleinern. Darüber hinaus finden Sie in der Werkzeugleiste mit{' '}
-								<Icon name="arrows-h" /> <i>"an Fensterbreite anpassen"</i> und{' '}
-								<span style={{ whiteSpace: 'nowrap' }}>
-									<Icon name="arrows-v" /> <i>"an Fensterhöhe anpassen"</i>
-								</span>{' '}
-								zwei Möglichkeiten zur schnellen Optimierung der Dokumentdarstellung. Wenn Sie für
-								weitere B-Pläne aus dem Kartenfenster zur Dokumentbetrachtung wechseln, wird eine ggf.
-								bereits zuvor für den Dokumentenviewer geöffnete Registerkarte Ihres Browsers
-								angesteuert.
-							</p>
+							Durch Anklicken des Links "Dokumente" in der Info-Box oder des PDF-Symbols direkt darüber
+							wird in einer neuen Registerkarte Ihres Browsers ein Dokumentenviewer geöffnet, in dem die
+							Dokumente zu demjenigen B-Plan betrachtet werden können, der gerade den Fokus hat. Wenn zu
+							dem B-Plan mehrere Dokumente verfügbar sind, werden diese in einer Navigationsleiste am
+							linken Rand des Dokumentenviewers angeboten. Klicken Sie auf eines der Symbole in der
+							Navigationsleiste, um das zugehörige Dokument in den Anzeigebereich des Dokumentenviewers zu
+							laden. Mit den Werkzeugen <Icon name="chevron-left" /> <i>"vorherige Seite"</i> und{' '}
+							<Icon name="chevron-right" /> <i>"nächste Seite"</i> in der Werkzeugleiste am oberen Rand
+							des Dokumentenviewers können Sie in mehrseitigen Dokumenten vor- und zurückblättern.
+							<br />
+							Mehrere Dokumente werden angezeigt, wenn der B-Plan mehrere Planteile umfasst oder wenn die
+							verfahrensbegleitenden Zusatzdokumente bereits in digitaler Form vorliegen. Die
+							Bereitstellung dieser Dokumente ist ein laufendes Vorhaben der Stadtverwaltung Wuppertal,
+							Stand 03/2019 sind sie für rund 60% der Wuppertaler B-Pläne verfügbar. Die
+							Namenskonventionen für die vielfältigen Zusatzdateien sind im Dokument{' '}
+							<a href={INFO_DOC_DATEINAMEN_URL} target="_info">
+								Info Dateinamen
+							</a>{' '}
+							beschrieben, das in der Navigationsleiste als oberstes Zusatzdokument angeboten wird.
+							<br />
+							Im Anzeigebereich können Sie das Dokument durch Ziehen mit der Maus verschieben. Mit den
+							Werkzeugen <Icon name="plus" /> und <Icon name="minus" /> können Sie die Darstellung
+							vergrößern bzw. verkleinern. Darüber hinaus finden Sie in der Werkzeugleiste mit{' '}
+							<Icon name="arrows-h" /> <i>"an Fensterbreite anpassen"</i> und{' '}
+							<span style={{ whiteSpace: 'nowrap' }}>
+								<Icon name="arrows-v" /> <i>"an Fensterhöhe anpassen"</i>
+							</span>{' '}
+							zwei Möglichkeiten zur schnellen Optimierung der Dokumentdarstellung. Wenn Sie für weitere
+							B-Pläne aus dem Kartenfenster zur Dokumentbetrachtung wechseln, wird eine ggf. bereits zuvor
+							für den Dokumentenviewer geöffnete Registerkarte Ihres Browsers angesteuert.
+							<br />
 						</Panel>
 						<Panel header="B-Pläne herunterladen" eventKey="8" bsStyle="info">
-							<p>
-								Das Herunterladen der Dokumente zu einem B-Plan-Verfahren erfolgt aus dem
-								Dokumentenviewer. Dazu finden Sie in der Werkzeugleiste zwei Möglichkeiten. Mit dem
-								immer verfügbaren Werkzeug <Icon name="download" />{' '}
-								<i>"Dokument herunterladen (pdf)"</i>
-								können Sie das aktuell im Anzeigebereich dargestellte Dokument als PDF-Datei
-								herunterladen. Das zweite Werkzeug<Icon name="file-archive-o" />{' '}
-								<i>"alles herunterladen (zip)"</i>dient zum Download eines zip-Archivs mit allen
-								Planteilen und verfahrensbegleitenden Zusatzdokumenten einschließlich des Info-Dokuments
-								zu den Namenskonventionen für die Zusatzdokumente. Es lässt sich nur aktivieren, wenn zu
-								dem aktuell betrachteten B-Plan mehrere Dokumente verfügbar sind.
-							</p>
-							<p>
-								Unten rechts im Anzeigebereich finden Sie stets den Dateinamen, unter dem das gerade
-								dargestellte Dokument heruntergeladen wird. Diese Dateinamen sind etwas länger als die
-								in der Navigationsleiste verwendeten Bezeichnungen, wo die für alle Dokumente eines
-								B-Plans gleichen Namensbestandteile ausgeblendet werden.
-							</p>
-							<p>
-								Ob eine heruntergeladene Datei nach dem Download sofort mit einem geeigneten Programm
-								(PDF-Viewer oder Dateimanager) geöffnet wird, hängt von Ihren Betriebssystem- und/oder
-								Browsereinstellungen ab.
-							</p>
+							Das Herunterladen der Dokumente zu einem B-Plan-Verfahren erfolgt aus dem Dokumentenviewer.
+							Dazu finden Sie in der Werkzeugleiste zwei Möglichkeiten. Mit dem immer verfügbaren Werkzeug{' '}
+							<Icon name="download" /> <i>"Dokument herunterladen (pdf)"</i> können Sie das aktuell im
+							Anzeigebereich dargestellte Dokument als PDF-Datei herunterladen. Das zweite Werkzeug{' '}
+							<Icon name="file-archive-o" /> <i>"alles herunterladen (zip)"</i> dient zum Download eines
+							zip-Archivs mit allen Planteilen und verfahrensbegleitenden Zusatzdokumenten einschließlich
+							des Info-Dokuments zu den Namenskonventionen für die Zusatzdokumente. Es lässt sich nur
+							aktivieren, wenn zu dem aktuell betrachteten B-Plan mehrere Dokumente verfügbar sind.
+							<br />
+							Unten rechts im Anzeigebereich finden Sie stets den Dateinamen, unter dem das gerade
+							dargestellte Dokument heruntergeladen wird. Diese Dateinamen sind etwas länger als die in
+							der Navigationsleiste verwendeten Bezeichnungen, wo die für alle Dokumente eines B-Plans
+							gleichen Namensbestandteile ausgeblendet werden.
+							<br />
+							Ob eine heruntergeladene Datei nach dem Download sofort mit einem geeigneten Programm
+							(PDF-Viewer oder Dateimanager) geöffnet wird, hängt von Ihren Betriebssystem- und/oder
+							Browsereinstellungen ab.
 						</Panel>
 					</Accordion>
 				</Modal.Body>

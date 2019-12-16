700c9571d638a8598178fd220c759bd4ec2c742e
helllth <th@cismet.de>
2019-12-16 16:24:12 +0100

    :pencil: wip on #39: emob  TM. improved help text due to customer request

--- src/components/emob/ModalMenuHelpSection.js
+++ src/components/emob/ModalMenuHelpSection.js
@@ -52,7 +52,9 @@ const HelpSection = ({ uiState, uiStateActions }) => {
 							style={{ textDecoration: 'none' }}
 						>
 							{' '}
-							<Label bsStyle='success'>Anlagen auswählen und abfragen</Label>{' '}
+							<Label bsStyle='success'>
+								Ladestationen auswählen und abfragen
+							</Label>{' '}
 						</Link>
 
 						<Link
@@ -216,7 +218,7 @@ const HelpSection = ({ uiState, uiStateActions }) => {
 						<br />
 					</div>
 					<h4>
-						Anlagen auswählen und abfragen{' '}
+						Ladestationen auswählen und abfragen{' '}
 						<Link
 							id='lnkUpInHelp_auswahl'
 							to='help'
@@ -227,44 +229,45 @@ const HelpSection = ({ uiState, uiStateActions }) => {
 						</Link>
 					</h4>
 					<p>
-						Bewegen Sie den Mauszeiger im Kartenfenster auf eines der Symbole für
-						Park+Ride- bzw. Bike+Ride-Anlagen (P+R- bzw. B+R-Anlagen), um sich den Namen
-						der Anlage anzeigen zu lassen. Ein Klick auf das Symbol setzt den Fokus auf
-						diese Anlage. Sie wird dann blau hinterlegt und die zugehörigen
-						Informationen (Name, Lagebeschreibung, Anzahl der Plätze) werden in der
-						Info-Box angezeigt. (Auf einem Tablet-PC wird der Fokus durch das erste
-						Antippen des Symbols gesetzt, das zweite Antippen blendet den Namen ein.)
-						Durch Anklicken des Symbols <FontAwesomeIcon icon={faInfoCircle} /> rechts
-						neben dem Namen der Anlage öffnen Sie das Datenblatt mit den vollständigen
-						Informationen zu dieser Anlage einschließlich einer Verknüpfung zur
-						Fahrplanauskunft des VRR für die zugehörige Haltestelle. Mit dem Lupensymbol{' '}
-						<FontAwesomeIcon icon={faSearchLocation} /> wird die Karte auf die Anlage,
-						die gerade den Fokus hat, zentriert und gleichzeitig ein großer
-						Betrachtungsmaßstab (Zoomstufe 14) eingestellt.{' '}
+						Bewegen Sie den Mauszeiger im Kartenfenster auf eines der Symbole für die
+						Ladestationen (online oder offline), um sich den Namen der Station anzeigen
+						zu lassen. Ein Klick auf das Symbol setzt den Fokus auf diese Ladestation.
+						Sie wird dann blau hinterlegt und die zugehörigen Informationen (Name, ggf.
+						Lagebeschreibung, Adresse) werden in der Info-Box angezeigt. (Auf einem
+						Tablet-PC wird der Fokus durch das erste Antippen des Symbols gesetzt, das
+						zweite Antippen blendet den Namen ein.) Durch Anklicken des Symbols{' '}
+						<Icon name='info' /> rechts neben dem Namen der Ladestation öffnen Sie das
+						Datenblatt mit den vollständigen Informationen zu dieser Station
+						einschließlich einer Verknüpfung zur Ladekosten-Information des Betreibers.
+						Mit den Symbolen <Icon name='phone' /> und{' '}
+						<Icon name='external-link-square' /> rechts daneben können Sie den Betreiber
+						via Smartphone direkt anrufen oder zu seiner Website wechseln.
 					</p>
 					<p>
-						Wenn Sie noch keine Anlage im aktuellen Kartenausschnitt selektiert haben,
-						wird der Fokus automatisch auf die nördlichste Anlage gesetzt. Mit den
-						Funktionen <img alt='Cluster' src='images/vorher_treffer.png' /> vorheriger
-						Treffer und <img alt='Cluster' src='images/nachher_treffer.png' /> nächster
-						Treffer können Sie ausgehend von der Anlage, auf der gerade der Fokus liegt,
-						in nördlicher bzw. südlicher Richtung alle aktuell im Kartenfenster
-						angezeigten Anlagen durchmustern.
+						Wenn Sie noch keine Ladestation im aktuellen Kartenausschnitt selektiert
+						haben, wird der Fokus automatisch auf die nördlichste Station gesetzt. Mit
+						den Funktionen <img alt='Cluster' src='images/vorher_treffer.png' />{' '}
+						vorheriger Treffer und{' '}
+						<img alt='Cluster' src='images/nachher_treffer.png' /> nächster Treffer
+						können Sie ausgehend von der Ladestation, auf der gerade der Fokus liegt, in
+						nördlicher bzw. südlicher Richtung alle aktuell im Kartenfenster angezeigten
+						Stationen durchmustern.
 					</p>
 					<p>
 						Mit der Schaltfläche <Icon name='chevron-circle-down' /> im dunkelgrau
 						abgesetzten rechten Rand der Info-Box lässt sich diese so verkleinern, dass
-						nur noch der Typ der Anlage (Park + Ride oder Bike + Ride), ihr Name und die
-						Symbole <FontAwesomeIcon icon={faSearchLocation} /> und{' '}
-						<FontAwesomeIcon icon={faInfoCircle} /> angezeigt werden - nützlich für
-						Endgeräte mit kleinem Display. Mit der Schaltfläche{' '}
-						<Icon name='chevron-circle-up' /> an derselben Stelle können Sie die
-						Info-Box wieder vollständig einblenden.
+						nur noch der Betriebszustand der Ladestation (Ladestation online oder
+						offline), ihr Name und die Symbole <Icon name='info' />,{' '}
+						<Icon name='phone' /> und <Icon name='external-link-square' />
+						angezeigt werden - nützlich für Endgeräte mit kleinem Display. Mit der
+						Schaltfläche <Icon name='chevron-circle-up' /> an derselben Stelle können
+						Sie die Info-Box wieder vollständig einblenden.
 					</p>
 					<p>
-						Ein kleines Foto über der Info-Box vermittelt Ihnen einen Eindruck vom
-						Aussehen der Anlage vor Ort. Klicken Sie auf dieses Vorschaubild, um einen
-						Bildbetrachter ("Leuchtkasten") mit dem Foto zu öffnen.
+						Ein kleines Foto über der Info-Box vermittelt Ihnen bei den meisten
+						Ladestationen einen Eindruck vom Aussehen der Station vor Ort. Klicken Sie
+						auf dieses Vorschaubild, um einen Bildbetrachter ("Leuchtkasten") mit dem
+						Foto zu öffnen.
 					</p>
 
 					<div id='anchorDivInHelp_positionieren' name='InKartePositionieren'>

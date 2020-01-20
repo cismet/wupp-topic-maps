15d3f6ee98c6ca168599ecc3064e06eb83a1a7fd
helllth <th@cismet.de>
2020-01-20 16:01:57 +0100

    :pencil2: wip on #39: minor text issues

--- src/components/bplaene/Help30BPlaeneSuchen.js
+++ src/components/bplaene/Help30BPlaeneSuchen.js
@@ -11,15 +11,19 @@ const Component = ({ uiState, uiStateActions }) => {
 			sectionBsStyle='success'
 			sectionContent={
 				<div>
-					F&uuml;r Detailinformation, Dokumentbetrachtung und Download m&uuml;ssen Sie
-					zun&auml;chst nach B-Pl&auml;nen suchen. Die Treffer werden automatisch geladen
-					und in der Karte als transparente farbige Fl&auml;chen mit der B-Plan-Nummer in
-					jeder Teilfl&auml;che dargestellt (Geltungsbereiche der B-Pl&auml;ne).
-					<br />
-					Gr&uuml;ne Fl&auml;chen/Nummern stehen f&uuml;r rechtswirksame Verfahren, rote
-					Fl&auml;chen/Nummern f&uuml;r laufende. Eine gr&uuml;ne Fl&auml;che mit roter
-					Nummer bedeutet, dass es unter dieser Nummer ein rechtswirksames und ein
-					laufendes Verfahren gibt, die genau dasselbe Gebiet abdecken.
+					<p>
+						F&uuml;r Detailinformation, Dokumentbetrachtung und Download m&uuml;ssen Sie
+						zun&auml;chst nach B-Pl&auml;nen suchen. Die Treffer werden automatisch
+						geladen und in der Karte als transparente farbige Fl&auml;chen mit der
+						B-Plan-Nummer in jeder Teilfl&auml;che dargestellt (Geltungsbereiche der
+						B-Pl&auml;ne).
+					</p>
+					<p>
+						Gr&uuml;ne Fl&auml;chen/Nummern stehen f&uuml;r rechtswirksame Verfahren,
+						rote Fl&auml;chen/Nummern f&uuml;r laufende. Eine gr&uuml;ne Fl&auml;che mit
+						roter Nummer bedeutet, dass es unter dieser Nummer ein rechtswirksames und
+						ein laufendes Verfahren gibt, die genau dasselbe Gebiet abdecken.
+					</p>
 				</div>
 			}
 		/>
--- src/components/bplaene/Help40SucheImKartenausschnitt.js
+++ src/components/bplaene/Help40SucheImKartenausschnitt.js
@@ -3,35 +3,40 @@ import GenericModalMenuSection from '../commons/GenericModalMenuSection';
 import Icon from 'components/commons/Icon';
 
 const Component = ({ uiState, uiStateActions }) => {
-  return (
-    <GenericModalMenuSection
-      uiState={uiState}
-      uiStateActions={uiStateActions}
-      sectionKey="suche-im-kartenausschnitt"
-      sectionTitle="Suche im Kartenausschnitt"
-      sectionBsStyle="success"
-      sectionContent={
-        <div>
-          Durch Anklicken von&nbsp;
-          <Icon name="search" />
-          &nbsp;suchen Sie nach B-Pl&auml;nen, die zumindest teilweise im aktuellen Kartenausschnitt
-          liegen.
-          <br />
-          Den Kartenausschnitt k&ouml;nnen Sie durch Ziehen mit der Maus verschieben. Mit den
-          Werkzeugen&nbsp;
-          <Icon name="plus" />
-          &nbsp;und&nbsp;
-          <Icon name="minus" />
-          &nbsp;k&ouml;nnen Sie den Kartenma&szlig;stab vergr&ouml;&szlig;ern bzw. verkleinern.
-          <br />
-          Mit einem Doppelklick auf einen B-Plan in der Hintergrundkarte werden alle B-Pl&auml;ne
-          geladen, die an dieser Stelle liegen - meistens genau einer, manchmal auch mehrere
-          Pl&auml;ne. Ein Doppelklick auf die Hintergrundkarte <b>au&szlig;erhalb</b> der
-          angezeigten B-Pl&auml;ne entfernt alle zuvor geladenen B-Pl&auml;ne (Zur&uuml;cksetzen der
-          Suche).
-        </div>
-      }
-    />
-  );
+	return (
+		<GenericModalMenuSection
+			uiState={uiState}
+			uiStateActions={uiStateActions}
+			sectionKey='suche-im-kartenausschnitt'
+			sectionTitle='Suche im Kartenausschnitt'
+			sectionBsStyle='success'
+			sectionContent={
+				<div>
+					<p>
+						Durch Anklicken von&nbsp;
+						<Icon name='search' />
+						&nbsp;suchen Sie nach B-Pl&auml;nen, die zumindest teilweise im aktuellen
+						Kartenausschnitt liegen.
+					</p>
+					<p>
+						Den Kartenausschnitt k&ouml;nnen Sie durch Ziehen mit der Maus verschieben.
+						Mit den Werkzeugen&nbsp;
+						<Icon name='plus' />
+						&nbsp;und&nbsp;
+						<Icon name='minus' />
+						&nbsp;k&ouml;nnen Sie den Kartenma&szlig;stab vergr&ouml;&szlig;ern bzw.
+						verkleinern.
+					</p>
+					<p>
+						Mit einem Doppelklick auf einen B-Plan in der Hintergrundkarte werden alle
+						B-Pl&auml;ne geladen, die an dieser Stelle liegen - meistens genau einer,
+						manchmal auch mehrere Pl&auml;ne. Ein Doppelklick auf die Hintergrundkarte{' '}
+						<b>au&szlig;erhalb</b> der angezeigten B-Pl&auml;ne entfernt alle zuvor
+						geladenen B-Pl&auml;ne (Zur&uuml;cksetzen der Suche).
+					</p>
+				</div>
+			}
+		/>
+	);
 };
 export default Component;

8d2e43a53df8a17bccae58e2a05e9b7d20bc86b6
helllth <th@cismet.de>
2020-04-14 16:48:08 +0200

    :pencil2: wip on #41: help text updates due to customer request

--- src/components/fnp/help/Help50FlaechenInDerArbeitskarteAuswaehlenUndAbfragen.js
+++ src/components/fnp/help/Help50FlaechenInDerArbeitskarteAuswaehlenUndAbfragen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import GenericModalMenuSection from 'components/commons/GenericModalMenuSection';
 import Icon from 'components/commons/Icon';
 
-const Component = ({ uiState, uiStateActions }) => {
+const Component = ({ uiState, uiStateActions, showModalMenu }) => {
 	return (
 		<GenericModalMenuSection
 			uiState={uiState}
@@ -13,26 +13,58 @@ const Component = ({ uiState, uiStateActions }) => {
 			sectionContent={
 				<div>
 					<p>
-						Um ein B-Plan-Verfahren direkt anzusteuern, geben Sie den Anfang der
-						B-Plan-Nummer im Eingabefeld rechts neben&nbsp;
-						<Icon name='search' />
-						&nbsp;ein (mindestens 2 Ziffern). Alle Verfahren, die mit diesen Ziffern
-						beginnen, werden Ihnen in einer inkrementellen Auswahlliste angeboten. (Wenn
-						Sie weitere Zeichen eingeben, wird der Inhalt der Auswahlliste angepasst.)
+						In der Arbeitskarte zeigen wir Ihnen die fortgeschriebenen, flächenhaften
+						Hauptnutzungen des Flächennutzungsplans (FNP). Diese Flächen bilden auf dem
+						gesamten Wuppertaler Stadtgebiet ein lückenloses Mosaik. Mit einem
+						Doppelklick können Sie eine dieser Flächen auswählen - sie erhält dann
+						sofort den Fokus (Darstellung mit verstärktem schwarzem Umring und blasserer
+						Füllfarbe), sodass die zugehörigen Flächeninformationen in der Info-Box
+						angezeigt werden. Mit einem weiteren Klick auf diese Fläche wird der
+						Kartenausschnitt so angepasst, dass sie vollständig und zentriert
+						dargestellt wird. Ein Doppelklick auf eine Position außerhalb des
+						Wuppertaler Stadtgebiets setzt die Auswahl der Hauptnutzungsfläche zurück.
+						In der Info-Box werden dann wieder die anfänglichen, kurzen
+						Bedienungshinweise angezeigt.
 					</p>
 					<p>
-						Nach Auswahl eines B-Plan-Verfahrens aus dieser Liste wird
-						ausschlie&szlig;lich der zugeh&ouml;rige Plan geladen. Er wird
-						vollst&auml;ndig und zentriert dargestellt. Das ist vor allem n&uuml;tzlich,
-						um sich einen &Uuml;berblick &uuml;ber Pl&auml;ne mit einem komplizierten
-						Geltungsbereich zu verschaffen. (Probieren Sie mal die Nummer 150.)
+						Im farbigen Titelbalken der Info-Box wird die übergeordnete Kategorie der
+						jeweils ausgewählten Hauptnutzungsfläche angezeigt, z. B. "Fläche für den
+						Gemeinbedarf". Die Hintergrundfarbe des Titelbalkens greift dabei die
+						Farbsystematik der Kartendarstellung auf. Die Überschrift im Inneren der
+						Info-Box nennt die konkrete Zweckbestimmung der Fläche mit ihrer auf 0,1
+						Hektar gerundete Größe, z. B. "Schulische Zwecke (1,4 Ha)". Ein Hektar
+						entspricht ungefähr der Größe von 1,4 Fußballfeldern. Wenn die ausgewählte
+						Hauptnutzungsfläche im Rechtsplan mit einem punktförmigen Symbol dargestellt
+						ist, wird dieses rechts neben der Überschrift angezeigt.
 					</p>
 					<p>
-						Klicken Sie auf&nbsp;
-						<Icon name='search' />
-						&nbsp;, um alle Pl&auml;ne hinzuzuladen, die im jetzt aktuellen Ausschnitt
-						liegen. Damit stellen Sie auch sicher, dass Sie keinen Plan &uuml;bersehen,
-						der sich mit dem zuvor gesuchten &uuml;berlappt.
+						Zusätzlich finden Sie in der Info-Box noch die Angabe, seit wann die
+						ausgewählte Fläche rechtswirksam ist und wodurch die Festlegung erfolgte,
+						nämlich durch den FNP vom 17.01.2005 selbst oder ein FNP-Änderungsverfahren
+						(ÄV). Bei Hauptnutzungsflächen, die durch ein ÄV festgelegt wurden, wird die
+						Bezeichnung des ÄV und ggf. auch die Nummer des B-Plans, der Anlass für die
+						FNP-Änderung war, angegeben. Solche Hinweise auf Änderungsverfahren und
+						B-Pläne sind als Links ausgeführt, die den Dokumentenviewer mit den
+						zugehörigen Dokumenten öffnen (s.{' '}
+						<a onClick={() => showModalMenu('DokumenteBetrachten')}>
+							Dokumente betrachten
+						</a>)). Sofern sich die ausgewählte Hauptnutzungsfläche zu mindestens 80%
+						mit noch nicht rechtswirksamen ÄV überlappt, werden zusätzlich unter "<strong>s. auch</strong>"
+						Dokumentenviewer-Links auf diese ÄV angeboten. Auch in der Arbeitskarte
+						können Sie die Info-Box so verkleinern, dass nur noch der Titelbalken, die
+						Überschrift und ggf. das Symbol angezeigt werden (s.{' '}
+						<a onClick={() => showModalMenu('AenderungsverfahrenAnzeigenUndAbfragen')}>
+							Änderungsverfahren anzeigen und abfragen
+						</a>).
+					</p>
+					<p>
+						Der FNP enthält einige von der Bezirksregierung Düsseldorf als zuständiger
+						Aufsichtsbehörde mit ihren Verfügungen vom 14.10. und 02.12.2004, Az.
+						35.2-11.14 (Wup neu), nicht genehmigte Flächen, die in der Arbeitskarte als
+						weiße Flächen angezeigt werden. Sie sind im Rechtsplan und seiner Legende
+						von 1 bis 5 durchnummeriert. Nach einem Doppelklick auf eine solche Fläche
+						in der Arbeitskarte werden in der Info-Box spezifische Detailinformationen
+						zu dieser Fläche einschließlich ihrer Nummer im Rechtsplan angezeigt.
 					</p>
 				</div>
 			}

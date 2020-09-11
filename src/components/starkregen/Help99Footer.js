78cb68ef215bf5bef18c0a2eace1dfd6e3d5d551
helllth <th@cismet.de>
2020-09-11 15:33:25 +0200

    :pencil2: text updates due to customers request

--- src/components/starkregen/Help99Footer.js
+++ src/components/starkregen/Help99Footer.js
@@ -5,7 +5,7 @@ import CismetFooterAcks from 'components/commons/CismetFooterAcknowledgements';
 const Comp = ({ showModalMenu }) => {
 	return (
 		<div style={{ fontSize: '11px' }}>
-			<b>Hintergrundkarten</b>: True Orthophoto 2018, Amtliche Basiskarte (ABK), Hillshade ©
+			<b>Hintergrundkarten</b>: True Orthophoto 2020, Amtliche Basiskarte (ABK), Hillshade ©
 			Stadt Wuppertal | Stadtkarte 2.0 © RVR | WebAtlasDE © BKG{' '}
 			<a onClick={() => showModalMenu('datengrundlage')}>(Details und Nutzungsbedingungen)</a>
 			<br />

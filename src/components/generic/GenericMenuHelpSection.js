51d8d87f61fd91cc9b7c70ab7f5fd30485087b0e
helllth <th@cismet.de>
2020-10-22 09:03:13 +0200

    wo #50: corrected typo

--- src/components/generic/GenericMenuHelpSection.js
+++ src/components/generic/GenericMenuHelpSection.js
@@ -10,7 +10,7 @@ const DemoMenuHelpSection = ({ uiState, uiStateActions, markdown, content }) => 
 			uiState={uiState}
 			uiStateActions={uiStateActions}
 			sectionKey='help'
-			sectionTitle='Komnpaktanleitung'
+			sectionTitle='Kompaktanleitung'
 			sectionBsStyle='success'
 			sectionContent={content || <Markdown escapeHtml={false} source={markdown} />}
 		/>

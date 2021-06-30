d3dfb0ae2b5e8c22f63c2cdb5b5e511aa06b955e
helllth <th@cismet.de>
2021-06-30 13:55:32 +0200

    prettier changes and preload color config (additional mail functionality for staedtische Bäder

--- src/containers/Baeder.js
+++ src/containers/Baeder.js
@@ -8,159 +8,160 @@ import BaederModalMenu from '../components/baeder/BaederModalMenu';
 import InfoBoxFotoPreview from '../components/commons/InfoBoxFotoPreview';
 import TopicMap from '../containers/TopicMap';
 import {
-	actions as BaederActions,
-	getBadSvgSize,
-	getBaeder,
-	getBaederFeatureCollection,
-	getBaederFeatureCollectionSelectedIndex,
-	hasMinifiedInfoBox
+  actions as BaederActions,
+  getBadSvgSize,
+  getBaeder,
+  getBaederFeatureCollection,
+  getBaederFeatureCollectionSelectedIndex,
+  hasMinifiedInfoBox,
 } from '../redux/modules/baeder';
 import { actions as MappingActions } from '../redux/modules/mapping';
 import { actions as UIStateActions } from '../redux/modules/uiState';
+import { actions as stadtplanActions } from '../redux/modules/stadtplan';
 import { getColorForProperties } from '../utils/baederHelper';
 import { fotoKraemerCaptionFactory, fotoKraemerUrlManipulation } from '../utils/commonHelpers';
 import { featureHoverer, getFeatureStyler } from '../utils/stadtplanHelper';
 
 function mapStateToProps(state) {
-	return {
-		uiState: state.uiState,
-		mapping: state.mapping,
-		routing: state.routing,
-		baeder: state.baeder,
-		gazetteerTopics: state.gazetteerTopics
-	};
+  return {
+    uiState: state.uiState,
+    mapping: state.mapping,
+    routing: state.routing,
+    baeder: state.baeder,
+    gazetteerTopics: state.gazetteerTopics,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-	return {
-		mappingActions: bindActionCreators(MappingActions, dispatch),
-		uiStateActions: bindActionCreators(UIStateActions, dispatch),
-		routingActions: bindActionCreators(RoutingActions, dispatch),
-		baederActions: bindActionCreators(BaederActions, dispatch)
-	};
+  return {
+    mappingActions: bindActionCreators(MappingActions, dispatch),
+    uiStateActions: bindActionCreators(UIStateActions, dispatch),
+    routingActions: bindActionCreators(RoutingActions, dispatch),
+    baederActions: bindActionCreators(BaederActions, dispatch),
+    stadtplanActions: bindActionCreators(stadtplanActions, dispatch), //for the color rules
+  };
 }
 
 export class Baeder_ extends React.Component {
-	constructor(props, context) {
-		super(props, context);
-		this.gotoHome = this.gotoHome.bind(this);
-		this.changeMarkerSymbolSize = this.changeMarkerSymbolSize.bind(this);
-		this.props.mappingActions.setBoundingBoxChangedTrigger((bbox) =>
-			this.props.baederActions.refreshFeatureCollection(bbox)
-		);
-	}
-	componentDidMount() {
-		document.title = 'Bäderkarte Wuppertal';
-	}
-	gotoHome() {
-		if (this.topicMap) {
-			this.topicMap.wrappedInstance.gotoHome();
-		}
-	}
+  constructor(props, context) {
+    super(props, context);
+    this.gotoHome = this.gotoHome.bind(this);
+    this.changeMarkerSymbolSize = this.changeMarkerSymbolSize.bind(this);
+    this.props.mappingActions.setBoundingBoxChangedTrigger((bbox) =>
+      this.props.baederActions.refreshFeatureCollection(bbox)
+    );
+  }
+  componentDidMount() {
+    document.title = 'Bäderkarte Wuppertal';
+  }
+  gotoHome() {
+    if (this.topicMap) {
+      this.topicMap.wrappedInstance.gotoHome();
+    }
+  }
 
-	changeMarkerSymbolSize(size) {
-		this.props.baederActions.setBadSvgSize(size);
-		this.props.mappingActions.setFeatureCollectionKeyPostfix('MarkerSvgSize:' + size);
-	}
+  changeMarkerSymbolSize(size) {
+    this.props.baederActions.setBadSvgSize(size);
+    this.props.mappingActions.setFeatureCollectionKeyPostfix('MarkerSvgSize:' + size);
+  }
 
-	render() {
-		let info = (
-			<BaederInfo
-				key={
-					'BaederInfo.' +
-					(getBaederFeatureCollectionSelectedIndex(this.props.baeder) || 0)
-				}
-				pixelwidth={325}
-				featureCollection={getBaederFeatureCollection(this.props.baeder)}
-				items={getBaeder(this.props.baeder)}
-				selectedIndex={getBaederFeatureCollectionSelectedIndex(this.props.baeder) || 0}
-				next={() => {
-					this.props.baederActions.setSelectedFeatureIndex(
-						(getBaederFeatureCollectionSelectedIndex(this.props.baeder) + 1) %
-							getBaederFeatureCollection(this.props.baeder).length
-					);
-				}}
-				previous={() => {
-					this.props.baederActions.setSelectedFeatureIndex(
-						(getBaederFeatureCollectionSelectedIndex(this.props.baeder) +
-							getBaederFeatureCollection(this.props.baeder).length -
-							1) %
-							getBaederFeatureCollection(this.props.baeder).length
-					);
-				}}
-				fitAll={this.gotoHome}
-				showModalMenu={(section) =>
-					this.props.uiStateActions.showApplicationMenuAndActivateSection(true, section)}
-				uiState={this.props.uiState}
-				uiStateActions={this.props.uiStateActions}
-				panelClick={(e) => {}}
-				minified={hasMinifiedInfoBox(this.props.baeder)}
-				minify={(minified) => this.props.baederActions.setMinifiedInfoBox(minified)}
-			/>
-		);
-		let reduxBackground = undefined;
-		try {
-			reduxBackground = this.props.mapping.backgrounds[this.props.mapping.selectedBackground]
-				.layerkey;
-		} catch (e) {}
+  render() {
+    let info = (
+      <BaederInfo
+        key={'BaederInfo.' + (getBaederFeatureCollectionSelectedIndex(this.props.baeder) || 0)}
+        pixelwidth={325}
+        featureCollection={getBaederFeatureCollection(this.props.baeder)}
+        items={getBaeder(this.props.baeder)}
+        selectedIndex={getBaederFeatureCollectionSelectedIndex(this.props.baeder) || 0}
+        next={() => {
+          this.props.baederActions.setSelectedFeatureIndex(
+            (getBaederFeatureCollectionSelectedIndex(this.props.baeder) + 1) %
+              getBaederFeatureCollection(this.props.baeder).length
+          );
+        }}
+        previous={() => {
+          this.props.baederActions.setSelectedFeatureIndex(
+            (getBaederFeatureCollectionSelectedIndex(this.props.baeder) +
+              getBaederFeatureCollection(this.props.baeder).length -
+              1) %
+              getBaederFeatureCollection(this.props.baeder).length
+          );
+        }}
+        fitAll={this.gotoHome}
+        showModalMenu={(section) =>
+          this.props.uiStateActions.showApplicationMenuAndActivateSection(true, section)
+        }
+        uiState={this.props.uiState}
+        uiStateActions={this.props.uiStateActions}
+        panelClick={(e) => {}}
+        minified={hasMinifiedInfoBox(this.props.baeder)}
+        minify={(minified) => this.props.baederActions.setMinifiedInfoBox(minified)}
+      />
+    );
+    let reduxBackground = undefined;
+    try {
+      reduxBackground =
+        this.props.mapping.backgrounds[this.props.mapping.selectedBackground].layerkey;
+    } catch (e) {}
 
-		let selectedFeature = (getBaederFeatureCollection(this.props.baeder) || [ {} ])[
-			getBaederFeatureCollectionSelectedIndex(this.props.baeder) || 0
-		];
+    let selectedFeature = (getBaederFeatureCollection(this.props.baeder) || [{}])[
+      getBaederFeatureCollectionSelectedIndex(this.props.baeder) || 0
+    ];
 
-		return (
-			<TopicMap
-				ref={(comp) => {
-					this.topicMap = comp;
-				}}
-				initialLoadingText='Laden der Bäder'
-				fullScreenControl
-				locatorControl
-				gazetteerSearchBox
-				gazetteerSearchBoxPlaceholdertext='Stadtteil | Adresse | POI'
-				photoLightBox
-				infoBox={info}
-				secondaryInfoBoxElements={[
-					<InfoBoxFotoPreview
-						currentFeature={selectedFeature}
-						uiStateActions={this.props.uiStateActions}
-						urlManipulation={fotoKraemerUrlManipulation}
-						captionFactory={fotoKraemerCaptionFactory}
-					/>
-				]}
-				backgroundlayers={
-					this.props.match.params.layers || reduxBackground || 'wupp-plan-live'
-				}
-				dataLoader={this.props.baederActions.loadBaeder}
-				getFeatureCollectionForData={() => {
-					return getBaederFeatureCollection(this.props.baeder);
-				}}
-				featureStyler={getFeatureStyler(
-					getBadSvgSize(this.props.baeder) || 30,
-					getColorForProperties
-				)}
-				refreshFeatureCollection={this.props.baederActions.refreshFeatureCollection}
-				setSelectedFeatureIndex={this.props.baederActions.setSelectedFeatureIndex}
-				featureHoverer={featureHoverer}
-				applicationMenuTooltipString='Einstellungen | Kompaktanleitung'
-				modalMenu={
-					<BaederModalMenu
-						uiState={this.props.uiState}
-						uiStateActions={this.props.uiStateActions}
-						urlPathname={this.props.routing.location.pathname}
-						urlSearch={this.props.routing.location.search}
-						pushNewRoute={this.props.routingActions.push}
-						currentMarkerSize={getBadSvgSize(this.props.baeder)}
-						changeMarkerSymbolSize={this.changeMarkerSymbolSize}
-						topicMapRef={this.topicMap}
-						setLayerByKey={this.props.mappingActions.setSelectedMappingBackground}
-						activeLayerKey={this.props.mapping.selectedBackground}
-					/>
-				}
-				featureCollectionKeyPostfix={this.props.mapping.featureCollectionKeyPostfix}
-			/>
-		);
-	}
+    return (
+      <TopicMap
+        ref={(comp) => {
+          this.topicMap = comp;
+        }}
+        initialLoadingText="Laden der Bäder"
+        fullScreenControl
+        locatorControl
+        gazetteerSearchBox
+        gazetteerSearchBoxPlaceholdertext="Stadtteil | Adresse | POI"
+        photoLightBox
+        infoBox={info}
+        secondaryInfoBoxElements={[
+          <InfoBoxFotoPreview
+            currentFeature={selectedFeature}
+            uiStateActions={this.props.uiStateActions}
+            urlManipulation={fotoKraemerUrlManipulation}
+            captionFactory={fotoKraemerCaptionFactory}
+          />,
+        ]}
+        backgroundlayers={this.props.match.params.layers || reduxBackground || 'wupp-plan-live'}
+        dataLoader={[
+          this.props.baederActions.loadBaeder,
+          this.props.stadtplanActions.loadPOIColors,
+        ]}
+        getFeatureCollectionForData={() => {
+          return getBaederFeatureCollection(this.props.baeder);
+        }}
+        featureStyler={getFeatureStyler(
+          getBadSvgSize(this.props.baeder) || 30,
+          getColorForProperties
+        )}
+        refreshFeatureCollection={this.props.baederActions.refreshFeatureCollection}
+        setSelectedFeatureIndex={this.props.baederActions.setSelectedFeatureIndex}
+        featureHoverer={featureHoverer}
+        applicationMenuTooltipString="Einstellungen | Kompaktanleitung"
+        modalMenu={
+          <BaederModalMenu
+            uiState={this.props.uiState}
+            uiStateActions={this.props.uiStateActions}
+            urlPathname={this.props.routing.location.pathname}
+            urlSearch={this.props.routing.location.search}
+            pushNewRoute={this.props.routingActions.push}
+            currentMarkerSize={getBadSvgSize(this.props.baeder)}
+            changeMarkerSymbolSize={this.changeMarkerSymbolSize}
+            topicMapRef={this.topicMap}
+            setLayerByKey={this.props.mappingActions.setSelectedMappingBackground}
+            activeLayerKey={this.props.mapping.selectedBackground}
+          />
+        }
+        featureCollectionKeyPostfix={this.props.mapping.featureCollectionKeyPostfix}
+      />
+    );
+  }
 }
 
 const Baeder = connect(mapStateToProps, mapDispatchToProps, null, { withRef: true })(Baeder_);
@@ -168,6 +169,6 @@ const Baeder = connect(mapStateToProps, mapDispatchToProps, null, { withRef: tru
 export default Baeder;
 
 Baeder.propTypes = {
-	ui: PropTypes.object,
-	uiState: PropTypes.object
+  ui: PropTypes.object,
+  uiState: PropTypes.object,
 };
